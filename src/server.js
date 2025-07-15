1923d30e6e7d9367749d0c6c6b28eeca4499c456
mghebro <giorgimgebrishvili2008@gmail.com>
2025-07-15 20:46:52 +0400

    try again after indian guy video

--- src/server.js
+++ src/server.js
@@ -160,7 +160,7 @@ app.get("/", (req, res) => {
 });
 
 // CORRECTED ROUTE DEFINITION: This route handles the callback from Apple
-app.post("/api/AppleService/auth/apple-callback", async (req, res) => {
+app.post(CSHARP_BACKEND_API_URL, async (req, res) => {
   let userAppleId = null;
   let userEmail = null;
   let userName = null;
